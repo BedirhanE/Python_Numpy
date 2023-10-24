0ec6cae92c12690da666043e21416764bad67a35
Bedirhan Elçin <79866900+BedirhanE@users.noreply.github.com>
2023-10-24 14:03:58 +0300

    Update Project

--- numpy_ex.py
+++ numpy_ex.py
@@ -33,4 +33,35 @@ print(a)
 print("0 indexinden 3. indexe kadar olan veriler: ",a[0:3])
 print("başlangıç:Bitiş:artış miktarı na göre çekilen veriler:",a[0:8:2])  #0 dan 8 e kadar olan verileri 2 şer 2 şer olarak yazdıran kod.
 
-#
+#Koşullu eleman işlem
+v=np.array([1,2,3,4,5])
+print(v<3)
+
+print(v[v<3])
+print(v[v>3])
+print(v[v!=3])
+print(v[v==3])
+
+
+#Matematiksel işlemler
+print("v deki bütün elemanlardan 1 çıkarma :",np.subtract(v,1))
+
+print("her bir elemana 5 ekleme işlemi:",np.add(v,5))
+
+print("her bir elemanı 3 ile çarpma işlemi",np.multiply(v,3))
+
+print("her bir elemanı 5 e bölme işlemi:",np.divide(v,5))
+
+print("üslü değerini alma işlemi:",np.power(v,8))
+
+print("modunu alma işlemi:",np.mod(v,2))
+
+print("Mutlak değerini alma işlemi:",np.absolute(np.array([-3,5,-4-9])))
+
+print("trigonometrik fonk kullanma işlemi sin360 değeri: :",np.sin(360))
+
+print("v arrayı içerisindeki değerlerin log2 ye alınması işlemi:",np.log2(v))
+
+#Kullanmış olduğum Kütüphanenin düzenli olarak notlarına nasıl erişebilirim???
+#CHEATSHEET yapma işlemi ile
+#örn: numpcheatsheet ile google da aratma yaparak numpy kütüphanesinin notlarına  detaylı olarak ulaşabilirim
